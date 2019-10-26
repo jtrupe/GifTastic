4129901a3c0417d9035e07351d9b547c38a9114d
Julian Trupe <juliantrupe@gmail.com>
2019-10-26 09:40:07 -0700

    minor changes

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
-var giphyURL = "https://api.giphy.com/v1/gifs/search?"
+var giphyURL = "https://api.giphy.com/v1/gifs/search"
 var apiKey = "8MDlERoDZR0WwoC6kpJoFsZ172PuBJCw"
+
+$.ajax({
+    url: giphyURL +"&q=cars" + "&api_key=" + apiKey,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+})
